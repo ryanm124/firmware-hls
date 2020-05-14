f1e6be4acae9dfe1a76243a82c2e47cc3563eb30
Robert Glein <robert.glein@colorado.edu>
2020-05-14 10:23:08 -0600

    faulty 3D return value

--- IntegrationTests/ExpTopPRMEMC_Test/buildproject.sh
+++ IntegrationTests/ExpTopPRMEMC_Test/buildproject.sh
@@ -5,10 +5,10 @@ vivado_hls -f script_ME.tcl
 vivado_hls -f script_MC.tcl
 
 # Download needed data files to populate initial memories and compare final memories
-cd ../IntegrationTests/ExpTopPRMEMC_Test/emData
+cd ../emData/TC
 . download.sh
 
 # Open the Vivado GUI and initialize the project
-cd ../
+cd ../../IntegrationTests/ExpTopPRMEMC_Test
 vivado -mode tcl -source generate_project.tcl
 vivado -mode tcl -source synth.tcl
