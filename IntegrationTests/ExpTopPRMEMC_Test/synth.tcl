666148fe4da9adfb9ac4c4bad76421952bfeecdf
Robert Glein <robert.glein@colorado.edu>
2020-06-23 17:56:36 -0600

    add report_utilization_hierarchical.txt

--- IntegrationTests/ExpTopPRMEMC_Test/synth.tcl
+++ IntegrationTests/ExpTopPRMEMC_Test/synth.tcl
@@ -11,5 +11,7 @@ open_project ./${_xil_proj_name_}/${_xil_proj_name_}.xpr
 update_compile_order -fileset sources_1
 launch_runs synth_1 -jobs 4
 wait_on_run synth_1
+open_run synth_1
+report_utilization -hierarchical -file report_utilization_hierarchical.txt
 
 exit
