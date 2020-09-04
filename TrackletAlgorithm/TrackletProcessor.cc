7a12f5e27ed4edc2e3b075b71b9ca442fe654a31
Anders Ryd <ar322@cornell.edu>
2020-09-04 10:39:03 -0400

    very first code for TP

--- TrackletAlgorithm/TrackletProcessor.cc
+++ TrackletAlgorithm/TrackletProcessor.cc
@@ -1,7 +1,7 @@
-#include "TrackletCalculator.h"
+#include "TrackletProcessor.h"
 
 ////////////////////////////////////////////////////////////////////////////////
-// Top functions for various TrackletCalculators (TC). For each iteration of
+// Top functions for various TrackletProcessors (TP). For each iteration of
 // the main processing loop, a TC retrieves a pair of stub indices from one of
 // the stub-pair memories, and in turn, these indices are used to retrieve one
 // stub each from an inner and an outer all-stub memory. This pair of stubs is
@@ -10,11 +10,10 @@
 // projections to additional layers and disks are also calculated and are
 // written to the appropriate tracklet-projection memories.
 ////////////////////////////////////////////////////////////////////////////////
-void TrackletCalculator_L1L2E(
+void TrackletProcessor_L1L2D(
     const BXType bx,
     const AllStubMemory<BARRELPS> innerStubs[2],
-    const AllStubMemory<BARRELPS> outerStubs[1],
-    const StubPairMemory stubPairs[13],
+    const AllStubMemory<BARRELPS>* outerStubs,
     TrackletParameterMemory * trackletParameters,
     TrackletProjectionMemory<BARRELPS> projout_barrel_ps[TC::N_PROJOUT_BARRELPS],
     TrackletProjectionMemory<BARREL2S> projout_barrel_2s[TC::N_PROJOUT_BARREL2S],
@@ -28,20 +27,19 @@ void TrackletCalculator_L1L2E(
 #pragma HLS array_partition variable=projout_barrel_2s complete
 #pragma HLS array_partition variable=projout_disk complete
 
-TC_L1L2E: TrackletCalculator<
+TC_L1L2D: TrackletCalculator<
   TC::L1L2,
-  TC::E,
+  TC::D,
   InnerRegion<TC::L1L2>(),
   OuterRegion<TC::L1L2>(),
-  NASMemInner<TC::L1L2, TC::E>(),
-  NASMemOuter<TC::L1L2, TC::E>(),
-  NSPMem<TC::L1L2, TC::E>(),
+  NASMemInner<TC::L1L2, TC::D>(),
+  NASMemOuter<TC::L1L2, TC::D>(),
+  NSPMem<TC::L1L2, TC::D>(),
   kMaxProc
  >(
     bx,
     innerStubs,
     outerStubs,
-    stubPairs,
     trackletParameters,
     projout_barrel_ps,
     projout_barrel_2s,
@@ -49,81 +47,4 @@ TC_L1L2E: TrackletCalculator<
   );
 }
 
-void TrackletCalculator_L1L2G(
-    const BXType bx,
-    const AllStubMemory<BARRELPS> innerStubs[1],
-    const AllStubMemory<BARRELPS> outerStubs[2],
-    const StubPairMemory stubPairs[13],
-    TrackletParameterMemory * trackletParameters,
-    TrackletProjectionMemory<BARRELPS> projout_barrel_ps[TC::N_PROJOUT_BARRELPS],
-    TrackletProjectionMemory<BARREL2S> projout_barrel_2s[TC::N_PROJOUT_BARREL2S],
-    TrackletProjectionMemory<DISK> projout_disk[TC::N_PROJOUT_DISK]
-) {
-#pragma HLS inline recursive
-#pragma HLS resource variable=innerStubs.get_mem() latency=2
-#pragma HLS resource variable=outerStubs.get_mem() latency=2
-#pragma HLS resource variable=stubPairs.get_mem() latency=2
-#pragma HLS array_partition variable=projout_barrel_ps complete
-#pragma HLS array_partition variable=projout_barrel_2s complete
-#pragma HLS array_partition variable=projout_disk complete
-
-TC_L1L2G: TrackletCalculator<
-  TC::L1L2,
-  TC::G,
-  InnerRegion<TC::L1L2>(),
-  OuterRegion<TC::L1L2>(),
-  NASMemInner<TC::L1L2, TC::G>(),
-  NASMemOuter<TC::L1L2, TC::G>(),
-  NSPMem<TC::L1L2, TC::G>(),
-  kMaxProc
- >(
-    bx,
-    innerStubs,
-    outerStubs,
-    stubPairs,
-    trackletParameters,
-    projout_barrel_ps,
-    projout_barrel_2s,
-    projout_disk
-  );
-}
-
-void TrackletCalculator_L3L4E(
-    const BXType bx,
-    const AllStubMemory<BARRELPS> innerStubs[1],
-    const AllStubMemory<BARREL2S> outerStubs[2],
-    const StubPairMemory stubPairs[11],
-    TrackletParameterMemory * trackletParameters,
-    TrackletProjectionMemory<BARRELPS> projout_barrel_ps[TC::N_PROJOUT_BARRELPS],
-    TrackletProjectionMemory<BARREL2S> projout_barrel_2s[TC::N_PROJOUT_BARREL2S],
-    TrackletProjectionMemory<DISK> projout_disk[TC::N_PROJOUT_DISK]
-) {
-#pragma HLS inline recursive
-#pragma HLS resource variable=innerStubs.get_mem() latency=2
-#pragma HLS resource variable=outerStubs.get_mem() latency=2
-#pragma HLS resource variable=stubPairs.get_mem() latency=2
-#pragma HLS array_partition variable=projout_barrel_ps complete
-#pragma HLS array_partition variable=projout_barrel_2s complete
-#pragma HLS array_partition variable=projout_disk complete
-
-TC_L3L4E: TrackletCalculator<
-  TC::L3L4,
-  TC::E,
-  InnerRegion<TC::L3L4>(),
-  OuterRegion<TC::L3L4>(),
-  NASMemInner<TC::L3L4, TC::E>(),
-  NASMemOuter<TC::L3L4, TC::E>(),
-  NSPMem<TC::L3L4, TC::E>(),
-  kMaxProc
- >(
-    bx,
-    innerStubs,
-    outerStubs,
-    stubPairs,
-    trackletParameters,
-    projout_barrel_ps,
-    projout_barrel_2s,
-    projout_disk
-  );
-}
 ////////////////////////////////////////////////////////////////////////////////
