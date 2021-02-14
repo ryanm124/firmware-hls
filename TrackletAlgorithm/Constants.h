471a744996df4bd634353a189611fb7d7fea394a
Anders Ryd <ar322@cornell.edu>
2021-02-14 17:05:41 -0500

    Fixes to allign constants to emulation

--- TrackletAlgorithm/Constants.h
+++ TrackletAlgorithm/Constants.h
@@ -65,8 +65,8 @@ constexpr double kphi = 7.71867e-06;
 constexpr double kz = 0.0585938;
 
 // tracklet digitization constants
-constexpr double krinv = 1.02916e-06;
-constexpr double kphi0 = 1.54373e-05;
+constexpr double krinv = 1.04549e-06;
+constexpr double kphi0 = 1.56824e-05;
 constexpr double kt = 0.00195312;
 constexpr double kz0 = 0.0585938;
 
