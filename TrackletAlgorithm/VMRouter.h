fe849784878daf596a659f1544477cde637fb5ee
meholmbe <mei-li.holmberg@cern.ch>
2021-04-01 16:07:15 +0100

    updates and bug fixes so the VMR can run for all layers/disks.

--- TrackletAlgorithm/VMRouter.h
+++ TrackletAlgorithm/VMRouter.h
@@ -326,12 +326,6 @@ inline VMStubME<OutType> createStubME(const InputStub<InType> stub,
 
 	assert(rzfine >= 0);
 
-	int nFinePhiBits = stubME.getFinePhi().length(); // Number of bits used for fine phi
-
-	// Set finephi, i.e. the phi bits within a vme region region
-	auto finephi = iphivmFineBins<InType>(phiCorr,  nbitsvmlayer[Layer-1], nFinePhiBits);
-	stubME.setFinePhi(finephi);
-	  
 	return stubME;
 };
 
@@ -672,7 +666,7 @@ void VMRouter(const BXType bx, BXType& bx_o, const int fineBinTable[], const int
 		clear3DArray(nvmTE, addrCountTEO);
 	}
 
-	
+
 	/////////////////////////////////////
 	// Main Loop
 	constexpr int maxLoop = kMaxProc;
