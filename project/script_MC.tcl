d28e386d14a455912ca1d36e18186e689da07421
Andrew Hart <ahart@cern.ch>
2021-07-28 09:30:20 -0400

    Switch from PHIC to PHIB.

--- project/script_MC.tcl
+++ project/script_MC.tcl
@@ -22,19 +22,19 @@ source settings_hls.tcl
 # data files
 add_files -tb ../emData/MC/
 
-set_top MatchCalculator_L3PHIC
+set_top MatchCalculator_L3PHIB
 csynth_design
 export_design -format ip_catalog
 
-set_top MatchCalculator_L4PHIC
+set_top MatchCalculator_L4PHIB
 csynth_design
 export_design -format ip_catalog
 
-set_top MatchCalculator_L5PHIC
+set_top MatchCalculator_L5PHIB
 csynth_design
 export_design -format ip_catalog
 
-set_top MatchCalculator_L6PHIC
+set_top MatchCalculator_L6PHIB
 csynth_design
 export_design -format ip_catalog
 
